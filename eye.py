a3662757cc6f0465cec63ba912948389ee5674fc
Ori <45724082+therealOri@users.noreply.github.com>
2024-01-20 10:59:07 -0700

    Update eye.py

--- eye.py
+++ eye.py
@@ -3,6 +3,7 @@ from discord import app_commands
 import os
 import sqlite3
 import tomllib
+import re
 
 #Load our config
 with open('config.toml', 'rb') as fileObj:
@@ -11,6 +12,8 @@ with open('config.toml', 'rb') as fileObj:
 TOKEN = config["BOT_TOKEN"]
 GUILD_ID = config["GUILD_ID"]
 CHANNEL_ID = config["CHANNEL_ID"]
+
+attachment_types = config["attachment_types"]
 
 
 
@@ -35,20 +38,44 @@ def clear():
     os.system("clear||cls")
 
 
-def logger(message):
+async def logger(message):
+    if message.attachments:
+        for attachment in message.attachments:
+            if any(attachment.filename.lower().endswith(media) for media in attachment_types):
+                await attachment.save(f'attachments/{attachment.filename}')
+                print(f'Saved "{attachment.filename}" to directory "attachments"')
+            else:
+                pass
+        attachment = message.attachments[0]
+        attachment_url = attachment.url
+
+        cdn_url_regex = r"https?://cdn\.discordapp\.com/attachments/([0-9]+)/([0-9]+)/([A-Za-z0-9_.-]+)" #file names that will contain _ . or -
+        match = re.search(cdn_url_regex, attachment_url)
+        if match:
+            cdn_link = match.group(0)
+        else:
+            cdn_link = attachment_url
+    else:
+        cdn_link = ''
+
+    media=cdn_link
     author = str(message.author)
     content = str(message.content)
     userid = str(message.author.id)
     time = str(message.created_at)
     guild = str(message.guild)
     channel = str(message.channel)
-    row = [guild, channel, author, userid, time, content]
+    row = [guild, channel, author, userid, time, content, media]
     conn = sqlite3.connect('chat_logs.db')
     c = conn.cursor()
-    c.execute("INSERT INTO logs VALUES (?, ?, ?, ?, ?, ?)", row)
+    c.execute("INSERT INTO logs VALUES (?, ?, ?, ?, ?, ?, ?)", row)
     conn.commit()
     conn.close()
-    print(f"[{guild}] | [{channel}] | [@{author}] [{userid}] @ {time}: {content}")
+
+    if media:
+        print(f"[{guild}] | [{channel}] | [@{author}] [{userid}] @ {time}: {media}")
+    else:
+        print(f"[{guild}] | [{channel}] | [@{author}] [{userid}] @ {time}: {content}")
 
 
 @eye.event
@@ -66,15 +93,15 @@ async def on_message(message):
 
     # logs specified channel in guild.
     if message.guild.id == GUILD_ID and message.channel.id == CHANNEL_ID:
-        logger(message)
+        await logger(message)
 
     # logs messages in guild in all channels bot can see/read messages in.
     if message.guild.id == GUILD_ID and CHANNEL_ID == False:
-        logger(message)
+        await logger(message)
 
     # All guilds the bot is in and in all channels that the bot can read message in.
     if GUILD_ID == False and CHANNEL_ID == False:
-        logger(message)
+        await logger(message)
                 
 
 eye.run(TOKEN, reconnect=True)
